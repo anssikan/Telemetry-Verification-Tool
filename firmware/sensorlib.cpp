a48e0c49bfcfc081433f3e100fcf923ae09c1d5e
anssikan <anskangasv@gmail.com>
2026-04-05 15:05:59 +0300

    library done-ish

--- firmware/sensorlib.cpp
+++ firmware/sensorlib.cpp
@@ -1,31 +1,30 @@
-# include sensorlib.h
+#include "sensorlib.h"
 
-void DhtSensor::DhtSensor(uint8_t pin) : dhtPin(pin) {}
+DhtSensor::DhtSensor(uint8_t pin) : dhtPin(pin) {}
 
 void DhtSensor::start() {
     pinMode(dhtPin, INPUT_PULLUP);
 }
 
-bool DhtSensor::record(DhtCapture& record, uint8_t newLevel) {
-    if (cap.edgeCount >= EDGES) {
+bool DhtSensor::recordEdge(DhtRecord& record, uint8_t recordStatus) {
+    if (record.edgeCount >= EDGES) {
         // stop if too many edges recorded
-        record.status = CaptureStatus::manyedges;
-        record.captureEndUs = micros();
+        record.status = RecordStatus::manyEdges;
         return false;
     }
 
     // record new edge
     uint32_t timeNow  = micros();
     record.edges[record.edgeCount].offset = static_cast<uint16_t>(timeNow - record.startTime);
-    record.edges[record.edgeCount].level = newLevel;
+    record.edges[record.edgeCount].status = recordStatus;
     record.edgeCount++;
     return true;
 }
 
-DhtSensor::DhtCapture DhtSensor::capture() {
-    DhtCapture record{};
+DhtSensor::DhtRecord DhtSensor::recordTransaction() {
+    DhtRecord record{};
     record.startTime = micros();
-    record.status = CaptureStatus::ok;
+    record.status = RecordStatus::ok;
 
     // mcu gives low start signal at least 1 ms
     pinMode(dhtPin, OUTPUT);
@@ -34,28 +33,31 @@ DhtSensor::DhtCapture DhtSensor::capture() {
     pinMode(dhtPin, INPUT_PULLUP);
 
     // collect 
-    uint8_t firstLevel = digitalRead(dhtPin);
+    uint8_t firstStatus = digitalRead(dhtPin);
     uint32_t guardTime = micros();
 
     while(true) {
-        uint8_t secondLevel = digitalRead(dhtPin);
+        uint8_t secondStatus = digitalRead(dhtPin);
 
-        if (secondLevel != firstLevel) {
-            // edge happening
-            firstLevel = secondLevel;
+        if (secondStatus != firstStatus) {
+            // edge happening, set new status as current status
+            firstStatus = secondStatus;
 
-            // record
+            // record the edge, function does check for edge count
+            if (!recordEdge(record, firstStatus)) {
+                break;
+            }
+            guardTime = micros();
+
+            if (record.edgeCount >= EDGES) {
+                break;
+            }
         }
-
+        if ((micros() - guardTime) > 255) {
+            record.status = RecordStatus::timeout;
+            break;
+        }
     }
-
-
-
-
-
+    record.endTime = micros();
     return record;
 }
-
-
-
-
--- firmware/sensorlib.h
+++ firmware/sensorlib.h
@@ -1,30 +1,36 @@
+#ifndef SENSORLIB_H
+#define SENSORLIB_H
 
-class Sensorlib {
+#include <Arduino.h>
+
+class DhtSensor {
     public:
         static const uint8_t EDGES = 84;
 
-        enum class CaptureStatus : uint8_t {
+        enum class RecordStatus : uint8_t {
             ok,
             timeout,
-            manyedges
-        }
+            manyEdges
+        };
         struct EdgeEvent {
             uint16_t offset; // since startTime
             uint8_t status; // after edge, high or low
-        }
-        struct DhtCapture {
+        };
+        struct DhtRecord {
             uint64_t startTime;
             uint64_t endTime;
             uint8_t edgeCount;
-            CaptureStatus captureStatus;
-            EdgeEvent edges[EDGES]
-        }
+            RecordStatus status;
+            EdgeEvent edges[EDGES];
+        };
 
         explicit DhtSensor(uint8_t pin);
         void start();
-        DhtCapture capture();
+        DhtRecord recordTransaction();
     
     private:
         uint8_t dhtPin;
-        // functions
-}
+        bool recordEdge(DhtRecord& record, uint8_t recordStatus);
+};
+
+#endif
